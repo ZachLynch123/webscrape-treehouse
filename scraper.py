915afdab4b5a955d04b02ae94213203d8a03ff9d
HankBedford <Zachary.Lynch1337@gmail.com>
2019-01-11 15:14:44 -0800

    scraping all links in webpage

--- scraper.py
+++ scraper.py
@@ -6,14 +6,9 @@ html = urlopen('https://treehouse-projects.github.io/horse-land/index.html')
 
 soup = BeautifulSoup(html.read(), 'html.parser')
 
-featured_header = soup.find('div', {'class': 'featured'})
-print(featured_header.get_text())
-
-# find methon only finds the first, or specific element, find all finds all elements
-
-# parameters: name: name of element
-# attrs: looks for specific css class
-# recursive: all instences of tags. setting recursive to false will find only the direct children of the tag
-# string searches for strings rather than tags
-# **kwargs: searching out other items like css ids
-# limit: limits amount returned. for example, find is find_all with a limit of 1
+# since class is a reserved word in pyton, bs4 uses class_ to differentiacte between the two
+""" for button in soup.find(class_='button button--primary'):
+    print(button) """
+# finds all internal and external links in the webpage's ancher tags
+for link in soup.find_all('a'):
+    print(link.get('href'))
